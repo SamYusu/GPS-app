bef0db999a2b8cce959b99b18954eff3d8a34af6
Samir Mohamud <samir.mohamud.yusuf@gmail.com>
2020-05-28 14:30:57 +0200

    starten av appen del 1

--- Application/app/src/main/java/com/example/gps_login/MainActivity.java
+++ Application/app/src/main/java/com/example/gps_login/MainActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Password;
+    private Textview Attempts;
+    private Button Login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Attempts = (TextView)findViewById(R.id.tvAttempts);
+
     }
 }
